71f3b838ff782272a6567f72fb00a7a56545ad0d
JackB668 <54421731+JackB668@users.noreply.github.com>
2021-02-24 16:31:00 +1100

    Added template project and unit tests

---
+++ MyCustomMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace CodingExercises
+{
+    public static class MyCustomMethods
+    {
+        public static int AddTwoNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CodingExercises;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class MathTests
+        {
+            [TestMethod]
+            public void TestAddTwoPositiveInts()
+            {
+                const int firstNumber = 5;
+                const int secondNumber = 3;
+                const int expectedAnswer = 8;
+
+                Assert.AreEqual(expectedAnswer, MyCustomMethods.AddTwoNumbers(firstNumber, secondNumber), "Add two postive ints failed");
+            }
+        }
+    }
+}
